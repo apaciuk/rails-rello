76247a6ad5526c9d1a84289a935a6743f4412436
Paul Anthony McGowan <xhostcom@aol.com>
2022-10-08 06:04:39 +0100

    Validations & associations in specs

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe Board, type: :model do
   let(:board ) { FactoryBot.create(:board) }
   describe 'validations' do
-      it { is_expected.to belong_to :user }
-      #it { is_expected.to have_many :lists }
-      it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :name }
   end
 
- # describe 'associations' do
-  #  it { belongs_to(:user) }
-  #  it { has_many(:lists) }
- # end
+  describe 'associations' do 
+    it { is_expected.to belong_to :user }
+    # it { is_expected.to have_many(:lists).dependent(:destroy) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.create(:user) }
   describe "validations" do 
-    it "is valid with valid attributes" do
-      expect(user).to be_valid
-    end
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe "associations" do
+    it { is_expected.to have_many(:boards) }
+    #it { is_expected.to have_many(:lists).through(:boards) }
   end
 end
