6b6dc6243b4f1f952a2dd6338fd48191806cf840
Paul Anthony McGowan <xhostcom@aol.com>
2022-10-12 10:07:17 +0100

    tweak classes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -37,9 +37,10 @@ const initBoardSortable = (ulElements) => {
   ulElements.forEach((ul) => {
     new Sortable(ul, {
         dataIdAttr: 'data-id',
-        group: 'board', // set both lists to same group
-        draggable: ".item",
-        animation: 300
+        group: 'shared', // Share between lists
+        draggable: ".list-group-item",
+        animation: 150,
+        ghostClass: 'blue-background-class'
     });
   });
 };
